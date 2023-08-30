ee4d9b7d5e68b8ed599cda3609d28cba14c7d319
Anna Kochergina <akochergina92@gmail.com>
2023-08-30 21:22:36 +0300

    refactoring

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public int calculate (int price) {
+    public int calculate(int price) {
         int mile = 20;
         int reselt;
         if (price > 0) {
